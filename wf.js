5dec0e6d489e3a7ac21ba9b84061c62305f5326b
François Kooman <fkooman@tuxed.net>
2014-10-05 17:21:50 +0200

    use errorCallback

--- wf.js
+++ wf.js
@@ -1,50 +1,53 @@
 /* jshint browser:true, jquery:true */
 var WebFinger = function() {
-    'use strict';
+  'use strict';
 
-    return { 
-        _extractDomain: function(resource) {
-            var parsedResource = resource.match(/(\S+)@(\S+)/);
-            if (null === parsedResource) {
-                // did not match regexp
-                throw {
-                    name: 'WebFingerError',
-                    message: 'resource not valid'
-                };
+  return { 
+    _extractDomain: function(resource) {
+      var parsedResource = resource.match(/(\S+)@(\S+)/);
+      if (null === parsedResource) {
+        // did not match regexp
+        throw {
+          name: 'WebFingerError',
+          message: 'resource not valid'
+        };
+      }
+      return parsedResource[2];
+    },
+    finger: function(resource, callback, errorCallback) {
+      var domain = this._extractDomain(resource),
+        webFingerUri = 'https://' + 
+          domain + 
+          '/.well-known/webfinger?resource=acct:' + 
+          resource,
+        httpRequest = new XMLHttpRequest();
+
+      httpRequest.onreadystatechange = function() {
+        try { 
+          if (4 === httpRequest.readyState) {
+            if (200 === httpRequest.status) {
+              callback(httpRequest.responseText);
+            } else if (404 === httpRequest.status) {
+              throw {
+                name: 'WebFingerError',
+                message: 'resource not found'
+              };
+            } else {
+              throw {
+                name: 'WebFingerError',
+                message: 'there was a problem with the request'
+              };
             }
-            return parsedResource[2];
-        },
-        finger: function(resource, callback) {
-            var domain = this._extractDomain(resource),
-                webFingerUri = 'https://' + 
-                    domain + 
-                    '/.well-known/webfinger?resource=acct:' + 
-                    resource,
-                httpRequest = new XMLHttpRequest();
-
-            httpRequest.onreadystatechange = function() {
-                if (4 === httpRequest.readyState) {
-                    if (200 === httpRequest.status) {
-                        //callback(JSON.parse(httpRequest.responseText));
-                        callback(httpRequest.responseText);
-                    } else if (404 === httpRequest.status) {
-                        throw {
-                            name: 'WebFingerError',
-                            message: 'resource not found'
-                        };
-                    } else {
-                        throw {
-                            name: 'WebFingerError',
-                            message: 'there was a problem with the request'
-                        };
-                    }
-                }
-            };
-
-            httpRequest.open('GET', webFingerUri, true);
-            httpRequest.responseType = 'json';
-            httpRequest.setRequestHeader('Accept', 'application/jrd+json');
-            httpRequest.send();
+          }
+        } catch (e) {
+          errorCallback(e);
         }
-    };
+      };
+
+      httpRequest.open('GET', webFingerUri, true);
+      //httpRequest.responseType = 'json';
+      httpRequest.setRequestHeader('Accept', 'application/jrd+json');
+      httpRequest.send();
+    }
+  };
 }();
